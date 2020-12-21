d6b575005ad41e614da37e5920466bafa70a9887
Lauren Malley <laurenymalley@gmail.com>
2020-12-21 00:09:06 -0500

    testing actions22

--- .github/actions/publish/index.js
+++ .github/actions/publish/index.js
@@ -3,8 +3,15 @@ const github=require("@actions/github")
 
 try {
 //throw( new Error('some error message'))
-const name=core.getInput("who-to-greet")
-console.log(`Hello ${name}`)
+const message=JSON.stringify(github.event.commits[github.event.commits-1].message)
+const splitUp=message.split(' ')
+if (splitUp[0] === "Merge" && splitUp[1] === "pull" && splitUp[2] === "request") {
+    core.setOutput("mergeMessage", message)
+} else {
+    core.setFailed("Not valid merge")
+}
+// const name=core.getInput("who-to-greet")
+// console.log(`Hello ${name}`)
 
 const time= new Date()
 core.setOutput("time", time.toTimeString())
