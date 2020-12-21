e28502892d234d3bc993de3986cbdf57158c90a0
Lauren Malley <laurenymalley@gmail.com>
2020-12-21 16:46:14 -0500

    testing actions76

--- .github/actions/publish/index.js
+++ .github/actions/publish/index.js
@@ -1,37 +1,15 @@
 const core=require("@actions/core")
 const github=require("@actions/github")
 try {
-const event=core.getInput("github-object")
-const committerUsername=core.getInput("committer-username")
-  //  const context=JSON.stringify(github.context, null, '\t')
+//check for merge from pull request
 const committer=JSON.stringify(github.context.payload.commits[github.context.payload.commits.length-1].committer.username)
-console.log(committer === '"web-flow"')
-console.log("2nd", committer == "web-flow")
-console.log("here,", committer)
-    // core.setOutput("mergeMessage", JSON.stringify(github.context.payload.commits, null, '\t'))
-    if (committer === "web-flow") {
+    if (committer === '"web-flow"') {
     core.setOutput("mergeMessage", committer)
     } else {
         core.setFailed("Not a pr merge")
     }
-    // const message=commit.message
-    // console.log("msg", message)
-    // console.log("committer", commit.committer)
-    
-    // const splitUp=message.split(' ')
-    // //const first=splitUp[0]
-    // //const message=context.event.commits[context.event.commits-1].message
-    // //check for merge message
-    // //if (splitUp[0] ==='"testing') {
-    //     if (splitUp[0] === '"Merge' && splitUp[1] === "pull" && splitUp[2] === "request") {
-    
-    // core.setOutput("mergeMessage", message)
-    // } else {
-    //     core.setFailed("Not valid merge")
-    // }
-    // console.log("without quotes", message.split('"'))
-    // console.log("MESSAGE", splitUp)
-    // console.log(message)
+    //check name of pull request
+    console.log(JSON.stringify(github))
     } catch (error) {
     core.setFailed(error.message)
     }
