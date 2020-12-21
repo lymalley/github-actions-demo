89bb14689ad79738d9883ba83448e00d58896be9
Lauren Malley <laurenymalley@gmail.com>
2020-12-21 11:26:24 -0500

    testing actions51

--- .github/actions/publish/dist/index.js
+++ .github/actions/publish/dist/index.js
@@ -1942,8 +1942,8 @@ try {
 //throw( new Error('some error message'))
 // const gh=JSON.stringify(github)
 // core.setOutput("mergeMessage", gh)
-//const message=core.getInput("github-object")
-const message =JSON.stringify(github.context.payload.commits[github.context.payload.commits-1], null, '\t')
+const message=core.getInput("github-object")
+//const message =JSON.stringify(github.context.payload.commits[github.context.payload.commits-1], null, '\t')
 const splitUp=message.split(' ')
 //const first=splitUp[0]
 //const message=context.event.commits[context.event.commits-1].message
@@ -1954,7 +1954,7 @@ core.setOutput("mergeMessage", message)
 } else {
     core.setFailed("Not valid merge")
 }
-
+console.log("without quotes", message.split('"'))
 console.log("MESSAGE", splitUp)
 console.log(JSON.stringify(github.context.payload.commits[github.context.payload.commits-1], null, '\t'))
 } catch (error) {
