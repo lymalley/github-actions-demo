239bd1fb3e93ce321d6384f614646081e4d0909e
Lauren Malley <laurenymalley@gmail.com>
2020-12-21 11:45:54 -0500

    testing actions54

--- .github/actions/publish/dist/index.js
+++ .github/actions/publish/dist/index.js
@@ -1949,9 +1949,10 @@ const message=core.getInput("github-object")
 const splitUp=message.split(' ')
 //const first=splitUp[0]
 //const message=context.event.commits[context.event.commits-1].message
+//check for merge message
+//if (splitUp[0] ==='"testing') {
+    if (splitUp[0] === '"Merge' && splitUp[1] === "pull" && splitUp[2] === "request") {
 
-if (splitUp[0] ==='testing') {
-   // if (splitUp[0] == '"Merge' && splitUp[1] == "pull" && splitUp[2] == "request") {
 core.setOutput("mergeMessage", message)
 } else {
     core.setFailed("Not valid merge")
